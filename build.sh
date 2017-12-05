711d159f53fea954278cbdf2c3d0114926480be6
krae-kim <krae.kim@samsung.com>
2017-12-05 12:04:15 +0900

    Scripts for DP runtime are added

--- build.sh
+++ build.sh
@@ -64,31 +64,4 @@ do
 done
 echo "...$(ls ${output_path}/task | wc -l) task jars are extracted."
 
-function download_flink() {
-	source ${fw_repo}/docker_files/sources.version
-	FLINK_BASE_URL="$(curl -s https://www.apache.org/dyn/closer.cgi\?preferred\=true)flink/flink-${FLINK_VERSION}/"
-	FLINK_DIST_FILE_NAME="flink-${FLINK_VERSION}-bin-hadoop${HADOOP_VERSION}-scala_${SCALA_VERSION}.tgz"
-	CURL_OUTPUT="flink.tgz"
-
-	echo -ne "Downloading ${FLINK_DIST_FILE_NAME} from ${FLINK_BASE_URL}..."
-	cd ${cur_path}
-	curl -s ${FLINK_BASE_URL}${FLINK_DIST_FILE_NAME} --output ${CURL_OUTPUT}
-	is_extracted "flink.tgz"
-	tar -xzf flink.tgz
-	rm flink.tgz
-}
-
-#read -p "Do you want to download Flink (yes/no)? " yn
-#case $yn in
-#	[Nn]* ) echo "Please place Flink binaries in flink directory.";;
-#	* ) download_flink;;
-#esac
-#download_flink
-
-#echo -ne "Checking out jobmanager script..."
-#if [ ! -f "./flink/bin/jobmanager.sh" ]; then
-#	mv -f flink-${FLINK_VERSION} ${output_path}/flink
-#fi
-#is_extracted "${output_path}/flink/bin/jobmanager.sh"
-
 echo "Finished."
