cc2f110569097dcd512c83342bec13f8d01d0929
krae-kim <krae.kim@samsung.com>
2017-12-11 10:22:52 +0900

    SEC info deleted form build scripts

--- build_arm.sh
+++ build_arm.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-cur_path=$(cd "$(dirname "$0")" && pwd)
-rsc_path=${cur_path}/docker_files
-output_path=${cur_path}/docker_files/resources
+repo_path=$(cd "$(dirname "$0")" && pwd)
+rsc_path=${repo_path}/docker_files
+output_path=${repo_path}/docker_files/resources
 source ${rsc_path}/sources.version
 mkdir -p $output_path
 
-echo -ne "Downloading Kapacitor binaries..."
+function is_extracted() {
+	if [ -f "$1" ]; then
+		echo -ne "\033[33m"
+		echo -ne "done"
+		echo -e "\033[0m"
+	else
+		echo -ne "\033[31m"
+		echo -ne "failed; $1; No such file exists"
+		echo -e "\033[0m"
+		exit 404
+	fi
+	return
+}
+
+echo "Downloading Kapacitor binaries..."
 cd ${output_path}
 wget -O kapacitor.tar.gz https://dl.influxdata.com/kapacitor/releases/kapacitor-${KAPACITOR_VERSION}_linux_armhf.tar.gz
 tar -xzf kapacitor.tar.gz
 rm kapacitor.tar.gz
 rm kapacitor
 ln -s "kapacitor-${KAPACITOR_VERSION}"*/ kapacitor
+is_extracted ${output_path}/kapacitor/usr/bin/kapacitord
 
-echo "Setting necessary configurations and libraries..."
+echo -n "Setting necessary configurations and libraries..."
 export GOPATH=${output_path}
 go get -u go.uber.org/zap
 cp ${rsc_path}/kapacitor.conf ${output_path}/
+is_extracted ${output_path}/src/go.uber.org/zap/Makefile
