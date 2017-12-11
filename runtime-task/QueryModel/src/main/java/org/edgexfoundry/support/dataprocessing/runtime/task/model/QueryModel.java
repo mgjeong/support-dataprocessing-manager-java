5e6c018eaf00206b87d600ee227664ac507092cf
krae-kim <krae.kim@samsung.com>
2017-12-11 19:29:44 +0900

    Kapacitor runner for docker added

---
+++ runtime-task/QueryModel/src/main/java/org/edgexfoundry/support/dataprocessing/runtime/task/model/QueryModel.java
@@ -0,0 +1,4 @@
+package org.edgexfoundry.support.dataprocessing.runtime.task.model;
+
+public class QueryModel {
+}
---
+++ runtime/src/main/java/org/edgexfoundry/support/dataprocessing/runtime/engine/kapacitor/script/ScriptFactory.java
@@ -0,0 +1,12 @@
+package org.edgexfoundry.support.dataprocessing.runtime.engine.kapacitor.script;
+
+import org.edgexfoundry.support.dataprocessing.runtime.db.JobTableManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScriptMapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScriptMapper.class);
+    private static JobTableManager jobTableManager = null;
+
+
+}
