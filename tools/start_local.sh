55f669b48c8b81d1da72d6601e216a9aa82c9f13
Jay Oh <junghyun.oh@samsung.com>
2017-12-21 13:44:41 +0900

    [UPDATE] Upodate README file for the runtime and execution shell script
    Signed-off-by: Jay Oh <junghyun.oh@samsung.com>

--- tools/start_local.sh
+++ tools/start_local.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 source $(dirname "$0")/path.prefs
 
-##### Simple validation #####
-if [ ! -d "$flink_bin" ]; then
-	echo "$flink_bin does not exist."
-	echo "  Download Flink & extract into "se/" folder"
-	echo "  Or locate Flink into user-defined folder"
-	echo "  and update its path in the tools/path.prefs file"
-	exit 1
-fi
-
 ##### Build first #####
 $(dirname "$0")/build_local.sh
 
@@ -18,24 +9,6 @@ if [ ! $status -eq 0 ]; then
 	echo "Build failed."
 	exit 1
 fi
-
-##### Restart Flink #####
-echo "Stopping Apache Flink cluster..."
-$flink_bin/stop-cluster.sh
-
-echo "Removing Apache Flink logs..."
-rm $flink_bin/../log/*
-
-echo "Starting Apache Flink cluster..."
-$flink_bin/start-cluster.sh
-
-# trap ctrl-c 
-trap ctrl_c INT
-
-function ctrl_c() {
-	echo "Stopping Apache Flink cluster..."
-	$flink_bin/stop-cluster.sh
-}
 
 ##### Start Application #####
 echo "Starting Application..."
