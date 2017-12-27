cb363a6b00eb67fb85253ada7b8cd92a11a453ee
Hyunjun Kim <hyunjun2.kim@samsung.net>
2017-12-27 17:58:52 +0900

    Update unit test cases to check code coverages.

--- runtime-common/src/test/java/org/edgexfoundry/support/dataprocessing/runtime/util/JarLoaderTest.java
+++ runtime-common/src/test/java/org/edgexfoundry/support/dataprocessing/runtime/util/JarLoaderTest.java
@@ -1,8 +1,10 @@
 package org.edgexfoundry.support.dataprocessing.runtime.util;
 
+import org.edgexfoundry.support.dataprocessing.runtime.task.TaskModel;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -12,6 +14,7 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.powermock.api.mockito.PowerMockito.*;
 
@@ -22,15 +25,15 @@ public class JarLoaderTest {
   @Test
   public void jarLoaderTest() throws Exception {
 
-    mockStatic(URL.class);
+    mockStatic(URL.class, Mockito.RETURNS_DEEP_STUBS);
     mockStatic(File.class);
-    mockStatic(URLClassLoader.class);
+    mockStatic(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
 
     URL url = mock(URL.class);
     URI uri = mock(URI.class);
     File file = mock(File.class);
     Method method = mock(Method.class);
-    URLClassLoader classLoader = mock(URLClassLoader.class);
+    URLClassLoader classLoader = mock(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
 
     whenNew(URL.class).withAnyArguments().thenReturn(url);
     whenNew(File.class).withAnyArguments().thenReturn(file);
@@ -40,10 +43,7 @@ public class JarLoaderTest {
     when(file.length()).thenReturn((long) 1);
     when(file.toURI()).thenReturn(uri);
     when(uri.toURL()).thenReturn(url);
-    when(URLClassLoader.class.getDeclaredMethod(anyString(), URL.class)).thenReturn(method);
-//    when(method.setAccessible(true));
-//    method.setAccessible(true);
-//    method.invoke(this.classLoader, new Object[]{file.toURI().toURL()});
+    when(URLClassLoader.class.getDeclaredMethod(anyString(), any())).thenReturn(method);
 
     try {
       JarLoader jarloader = new JarLoader("Test.jar", classLoader);
@@ -52,4 +52,70 @@ public class JarLoaderTest {
       e.printStackTrace();
     }
   }
+
+  @Test
+  public void unloadJarTest() throws Exception {
+
+    mockStatic(URL.class, Mockito.RETURNS_DEEP_STUBS);
+    mockStatic(File.class);
+    mockStatic(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
+
+    URL url = mock(URL.class);
+    URI uri = mock(URI.class);
+    File file = mock(File.class);
+    Method method = mock(Method.class);
+    URLClassLoader classLoader = mock(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
+
+    whenNew(URL.class).withAnyArguments().thenReturn(url);
+    whenNew(File.class).withAnyArguments().thenReturn(file);
+    whenNew(URLClassLoader.class).withAnyArguments().thenReturn(classLoader);
+
+    when(file.exists()).thenReturn(true);
+    when(file.length()).thenReturn((long) 1);
+    when(file.toURI()).thenReturn(uri);
+    when(uri.toURL()).thenReturn(url);
+    when(URLClassLoader.class.getDeclaredMethod(anyString(), any())).thenReturn(method);
+
+    try {
+      JarLoader jarloader = new JarLoader("Test.jar", classLoader);
+      Assert.assertNotNull(jarloader);
+      jarloader.unloadJar();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+//  @Test
+//  public void createInstanceTest() throws Exception {
+//
+//    mockStatic(URL.class, Mockito.RETURNS_DEEP_STUBS);
+//    mockStatic(File.class);
+//    mockStatic(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
+//
+//    URL url = mock(URL.class);
+//    URI uri = mock(URI.class);
+//    File file = mock(File.class);
+//    Method method = mock(Method.class);
+//    Class<TaskModel> clazz = mock((Class < TaskModel>).getClass());
+//    URLClassLoader classLoader = mock(URLClassLoader.class, Mockito.RETURNS_DEEP_STUBS);
+//
+//    whenNew(URL.class).withAnyArguments().thenReturn(url);
+//    whenNew(File.class).withAnyArguments().thenReturn(file);
+//    whenNew(URLClassLoader.class).withAnyArguments().thenReturn(classLoader);
+//
+//    when(file.exists()).thenReturn(true);
+//    when(file.length()).thenReturn((long) 1);
+//    when(file.toURI()).thenReturn(uri);
+//    when(uri.toURL()).thenReturn(url);
+//    when(classLoader.loadClass(anyString())).thenReturn(clazz);
+//
+//    try {
+//      JarLoader jarloader = new JarLoader("Test.jar", classLoader);
+//      Assert.assertNotNull(jarloader);
+//      jarloader.getClassInstance(
+//        "org.edgexfoundry.support.dataprocessing.runtime.util.test");
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    }
+//  }
 }
