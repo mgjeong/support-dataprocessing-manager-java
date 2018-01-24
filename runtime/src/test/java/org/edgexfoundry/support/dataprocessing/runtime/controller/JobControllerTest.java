096255ecdcc6f53d20f6a543a137106d4bc1ea6c
Junsung Lim <junsung.lim@samsung.com>
2018-01-24 14:55:37 +0900

    Add job controller test

--- runtime/src/test/java/org/edgexfoundry/support/dataprocessing/runtime/controller/JobControllerTest.java
+++ runtime/src/test/java/org/edgexfoundry/support/dataprocessing/runtime/controller/JobControllerTest.java
@@ -1,12 +1,21 @@
 package org.edgexfoundry.support.dataprocessing.runtime.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.spy;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import org.edgexfoundry.support.dataprocessing.runtime.data.model.job.Job;
+import org.edgexfoundry.support.dataprocessing.runtime.data.model.job.JobState;
+import org.edgexfoundry.support.dataprocessing.runtime.data.model.job.JobState.State;
 import org.edgexfoundry.support.dataprocessing.runtime.data.model.workflow.Workflow;
+import org.edgexfoundry.support.dataprocessing.runtime.data.model.workflow.WorkflowData;
 import org.edgexfoundry.support.dataprocessing.runtime.db.JobTableManager;
 import org.edgexfoundry.support.dataprocessing.runtime.db.WorkflowTableManager;
+import org.edgexfoundry.support.dataprocessing.runtime.engine.flink.FlinkEngine;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +40,80 @@ public class JobControllerTest {
 
     Assert.assertNotNull(response);
     Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void testDeployWorkflow() throws Exception {
+    JobController jobController = spy(createJobController());
+    Workflow workflow = createSampleWorkflow();
+    WorkflowData workflowData = createSampleWorkflowData(workflow);
+    Job job = createSampleJob();
+    FlinkEngine engine = mock(FlinkEngine.class);
+
+    doReturn(engine).when(jobController).createEngine(any(), any());
+    doReturn(job).when(engine).create(any());
+    doReturn(job).when(engine).run(any());
+    doReturn(workflow).when(workflowTableManager).getWorkflow(workflow.getId());
+    doReturn(workflowData).when(workflowTableManager).doExportWorkflow(workflow);
+    doReturn(job).when(jobTableManager).addOrUpdateWorkflowJob(any());
+    doReturn(job.getState()).when(jobTableManager).addOrUpdateWorkflowJobState(any(), any());
+
+    ResponseEntity response = jobController.deployWorkflow(workflow.getId());
+    Assert.assertNotNull(response);
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void testStopJob() throws Exception {
+    JobController jobController = spy(createJobController());
+    Workflow workflow = createSampleWorkflow();
+    Job job = createSampleJob();
+    FlinkEngine engine = mock(FlinkEngine.class);
+
+    doReturn(engine).when(jobController).createEngine(any(), any());
+    doReturn(job).when(engine).stop(any());
+    doReturn(job).when(jobTableManager).getWorkflowJob(any());
+    doReturn(job.getState()).when(jobTableManager).addOrUpdateWorkflowJobState(any(), any());
+
+    ResponseEntity response = jobController.stopJob(workflow.getId(), job.getId());
+    Assert.assertNotNull(response);
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void testGetWorkflowJobs() throws Exception {
+    JobController jobController = createJobController();
+    Job job = createSampleJob();
+    List<Job> jobs = new ArrayList<>();
+    jobs.add(job);
+    doReturn(jobs).when(jobTableManager).listWorkflowJobs(1L);
+    // valid
+    ResponseEntity response = jobController.getWorkflowJobs(1L);
+    Assert.assertNotNull(response);
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    doReturn(null).when(jobTableManager).listWorkflowJobs(1L);
+    response = jobController.getWorkflowJobs(1L);
+    Assert.assertNotNull(response);
+    Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  private Job createSampleJob() {
+    Job job = new Job();
+    job.setId("1");
+    job.setWorkflowId(1L);
+    job.setState(new JobState());
+    job.getState().setState(State.RUNNING);
+    job.getState().setStartTime(System.currentTimeMillis());
+    return job;
+  }
+
+  private WorkflowData createSampleWorkflowData(Workflow workflow) {
+    WorkflowData data = new WorkflowData();
+    data.setWorkflowId(workflow.getId());
+    data.getConfig().put("targetHost", "localhost:5555");
+
+    return data;
   }
 
   private Workflow createSampleWorkflow() {
