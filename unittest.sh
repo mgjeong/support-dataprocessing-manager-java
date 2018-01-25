cc0305fa339db26acf0a88b63f414992b9022112
Kyungrae Kim <krae.kim@samsung.com>
2018-01-25 13:07:16 +0900

    skipping mvn test in ARM

--- unittest.sh
+++ unittest.sh
@@ -2,13 +2,23 @@
 cur_path=$(cd "$(dirname "$0")" && pwd)
 fw_repo="${cur_path}"
 
-cd $fw_repo
-mvn test
-status=$?
-if [ $status -eq 0 ]; then
-	echo "Unit tests are done."
-	exit 0
+function test_x86() {
+	cd $fw_repo
+	mvn test
+	status=$?
+	if [ $status -eq 0 ]; then
+		echo "Unit tests are done."
+		exit 0
+	else
+		echo "Unit test is failed."
+		exit 1
+	fi
+}
+
+arch=$(uname -m)
+if [[ "$arch" == *"arm"* ]] || [[ "$arch" == *"ARM"* ]]; then
+	echo "Test starts for ARM architecture"
 else
-	echo "Unit test is failed."
-	exit 1
+	echo "Test starts for x86 architecture"
+	test_x86
 fi
